9ba31767820edc6fd4792bf0ba92e74d815a2e43
swatipradhan <swagatikakpradhan@gmail.com>
2015-10-30 19:46:22 +0530

    add ex5.py

---
+++ script/EX5.py
@@ -0,0 +1,17 @@
+#More variables and printing
+my_name = "swati pradhan"
+my_age = 35
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = "black"
+my_teeth = "white"
+my_hair = "black"
+
+print "lets talk about %s" % my_name
+print "She's %d years old," % my_age
+print "She's %d inches tall," % my_height
+print "She's %d pound heavy," % my_weight
+print "Actually that's not too heavy,"
+print "She's got %s eyes and %s hair," % (my_eyes, my_hair)
+print "She's teeth are usually %s depending on the coffee," % my_teeth
+print "If I add %d, %d, and %d i get %d," % (my_age,my_height,my_weight,my_age + my_height + my_weight)
